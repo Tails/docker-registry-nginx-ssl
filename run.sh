1798249b1171592df9638b653d9dadf75fea88f9
Luuk de Waal Malefijt <luuk@positivemoney.org>
2016-05-12 15:50:32 +0200

    fix htpass gen

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # generate pass based on env vars
-htpasswd -b -c /etc/nginx/.htpasswd ${HTPASSWD_USER} ${HTPASSWD_PASS}
+htpasswd -b -c /etc/nginx/.htpasswd "${HTPASSWD_USER}" "${HTPASSWD_PASS}"
 
 # determine hostname at runtime, maybe according
 # to host specific bahaviour
