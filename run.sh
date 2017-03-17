0eadf9f2e41da999641663f788d0a8e0689a2112
Luuk de Waal Malefijt <luuk@positivemoney.org>
2017-03-17 15:04:20 +0100

    dont fail when nginx certificates already exist

--- run.sh
+++ run.sh
@@ -35,6 +35,7 @@ if [ "${ENV}" = "production" ]; then
 
   # install certs in nginx
   mkdir -p /etc/nginx/ssl/ && \
+  rm -rf /etc/nginx/ssl/docker-registry.{crt,key} && \
   ln -s "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" "/etc/nginx/ssl/docker-registry.crt" && \
   ln -s "/etc/letsencrypt/live/${DOMAIN}/privkey.pem" "/etc/nginx/ssl/docker-registry.key" && \
 
